9466f0b226492964f50aa69c902624a4e8100b9d
youngwolf16 <cssanjaykumar02@gmail.com>
2025-08-04 07:33:11 +0530

    Intro to C

---
+++ Intro to C/1_basic_structure.c
@@ -0,0 +1,13 @@
+/*
+    Program : Basic Structure of C
+    Language: C
+    Author  : S Sanjay Kumar
+
+*/
+
+#include<stdio.h> // header section : Includes the standard input-output library, which provides functions like printf and scanf
+
+int main() { // Entry point of the C program, where the actual logic is implemented
+
+    return 0; // Returns 0 to indicate successful execution of the program
+}
---
+++ Intro to C/2_Hello_World.c
@@ -0,0 +1,18 @@
+/*
+    Program : Simple Hello World program
+    Language: C
+    Author  : S Sanjay Kumar
+
+*/
+
+#include<stdio.h> // The #include preprocessor directive imports the <stdio.h> header file, which contains input/output functions like printf and scanf.
+
+int main() // The operating system runs the main function. If the program executes successfully, it returns 0; otherwise, it returns a non-zero value.
+
+{ // Beginning of the block-structured program
+
+    printf("Hello World!"); // Displays output on the screen
+
+    return 0; // Indicates successful program execution
+
+} // End of the block
